3112088991da4da4a2336ee4c2f62f38e5bffcf7
wlhunter00 <30416118+wlhunter00@users.noreply.github.com>
2021-04-05 11:10:44 -0500

    edit permissions

--- view.js
+++ view.js
@@ -16,8 +16,6 @@ $('.perm_info').append("Info");
 $('#perm_panel').prepend(select_user);
 $('#perm_panel').attr('filepath', '/C');
 
-$('.permbutton').append($('Permissions'));
-
 $('.perm_info').click(function () {
     const filepath = $('#perm_panel').attr('filepath');
     const username = $('#perm_panel').attr('username');
@@ -43,7 +41,7 @@ function make_file_element(file_obj) {
             <h3 id="${file_hash}_header">
                 <span class="oi oi-folder" id="${file_hash}_icon"/> ${file_obj.filename} 
                 <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
+                    <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> Edit Permissions
                 </button>
             </h3>
         </div>`)
@@ -63,7 +61,7 @@ function make_file_element(file_obj) {
         return $(`<div class='file'  id="${file_hash}_div">
             <span class="oi oi-file" id="${file_hash}_icon"/> ${file_obj.filename}
             <button class="ui-button ui-widget ui-corner-all permbutton" path="${file_hash}" id="${file_hash}_permbutton"> 
-                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> 
+                <span class="oi oi-lock-unlocked" id="${file_hash}_permicon"/> Edit Permissions
             </button>
         </div>`)
     }
